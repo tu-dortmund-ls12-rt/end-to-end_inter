c203359bbf7496a74e0e6b79bb0c88e78d2ff55c
Mario Günzel <mario@MacBook-Air-von-Mario.local>
2022-02-24 16:56:12 +0100

    automatic evaluation

--- auto.sh
+++ auto.sh
@@ -1,16 +1,37 @@
 #!/usr/bin/env bash
 
-nmb_processes=1 # number of concurrent processes
+nmb_processes="$1" # number of concurrent processes
+utils="50 60 70 80 90"
+nmb_tasksets=1000
+nmb_interchains=10000
 
-# Synthesis
-python3 main.py --switch 1 --util 50 -n 10 --bench 'waters'
-python3 main.py --switch 1 --util 50 -n 10 --bench 'uunifast'
 
-python3 main.py --switch 2 --bench 'waters'
-python3 main.py --switch 2 --bench 'uunifast'
+# Single ECU Synthesis
+for ut in $utils
+do
+  python3 main.py --switch=1 --util=$ut -number=$nmb_tasksets --bench='waters'
+  python3 main.py --switch=1 --util=$ut -number=$nmb_tasksets --bench='uunifast'
+done
 
 # Single ECU experiments
-python3 main.py --switch 3 --util 50 -n 10 --bench 'waters'
-python3 main.py --switch 3 --util 50 -n 10 --bench 'uunifast'
+for ut in $utils
+do
+  python3 main.py --switch=2 --util=$ut -number=$nmb_tasksets --bench='waters' --proc=$nmb_processes
+  python3 main.py --switch=2 --util=$ut -number=$nmb_tasksets --bench='uunifast' --proc=$nmb_processes
+done
+
+# Inter ECU Synthesis
+python3 main.py --switch=3 --number=$nmb_interchains --bench='waters'
+python3 main.py --switch=3 --number=$nmb_interchains --bench='uunifast'
 
 # Inter ECU experiments
+python3 main.py --switch=4 --bench='waters' --proc=$nmb_processes
+python3 main.py --switch=4 --bench='uunifast' --proc=$nmb_processes
+
+# Single ECU plotting
+python3 main.py --switch=5 --bench='waters'
+python3 main.py --switch=5 --bench='uunifast'
+
+# Inter ECU plotting
+python3 main.py --switch=6 --bench='waters'
+python3 main.py --switch=6 --bench='uunifast'
