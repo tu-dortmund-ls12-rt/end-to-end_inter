d648a54534ba06e557b95c7a670dd93082cc89b2
Mario Günzel <mario@wlan-240217.nbw.tue.nl>
2023-02-01 16:24:53 +0100

    another source

--- utilities/analysis_becker.py
+++ utilities/analysis_becker.py
@@ -1,5 +1,6 @@
 """End-to-End (e2e) Analysis by Becker et al.
 https://doi.org/10.1016/j.sysarc.2017.09.004
+https://doi.org/10.1109/RTCSA.2016.41
 
 (own implementation for maximum data age only)
 """
