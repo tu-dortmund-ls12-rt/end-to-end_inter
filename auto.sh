e5142f9dfdad7acf8a57cf1aa63588e8cdc2b7af
Mario Günzel <mario@wlan-240041.nbw.tue.nl>
2023-02-06 09:30:50 +0100

    uncommenting in auto.sh

--- auto.sh
+++ auto.sh
@@ -7,26 +7,26 @@ nmb_interchains=10000
 
 
 # Single ECU Synthesis
-#for ut in $utils
-#do
-#  python3 main.py --switch=1 --util=$ut --number=$nmb_tasksets --bench='waters' --proc=$nmb_processes
-#  python3 main.py --switch=1 --util=$ut --number=$nmb_tasksets --bench='uunifast' --proc=$nmb_processes
-#done
-#
-## Single ECU experiments
-#for ut in $utils
-#do
-#  python3 main.py --switch=2 --util=$ut --number=$nmb_tasksets --bench='waters' --proc=$nmb_processes
-#  python3 main.py --switch=2 --util=$ut --number=$nmb_tasksets --bench='uunifast' --proc=$nmb_processes
-#done
-#
-## Inter ECU Synthesis
-#python3 main.py --switch=3 --number=$nmb_interchains --bench='waters'
-#python3 main.py --switch=3 --number=$nmb_interchains --bench='uunifast'
-#
-## Inter ECU experiments
-#python3 main.py --switch=4 --bench='waters' --proc=$nmb_processes
-#python3 main.py --switch=4 --bench='uunifast' --proc=$nmb_processes
+for ut in $utils
+do
+ python3 main.py --switch=1 --util=$ut --number=$nmb_tasksets --bench='waters' --proc=$nmb_processes
+ python3 main.py --switch=1 --util=$ut --number=$nmb_tasksets --bench='uunifast' --proc=$nmb_processes
+done
+
+# Single ECU experiments
+for ut in $utils
+do
+ python3 main.py --switch=2 --util=$ut --number=$nmb_tasksets --bench='waters' --proc=$nmb_processes
+ python3 main.py --switch=2 --util=$ut --number=$nmb_tasksets --bench='uunifast' --proc=$nmb_processes
+done
+
+# Inter ECU Synthesis
+python3 main.py --switch=3 --number=$nmb_interchains --bench='waters'
+python3 main.py --switch=3 --number=$nmb_interchains --bench='uunifast'
+
+# Inter ECU experiments
+python3 main.py --switch=4 --bench='waters' --proc=$nmb_processes
+python3 main.py --switch=4 --bench='uunifast' --proc=$nmb_processes
 
 # Single ECU plotting
 python3 main.py --switch=5 --bench='waters'
